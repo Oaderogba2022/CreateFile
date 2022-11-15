d3fe353163c145cc2cffd6d4ec84d466a2efe2d5
Olamide Aderogba <G00401971@atu.ie>
2022-11-15 15:33:45 +0000

    Prompt User for filename

--- src/main/java/ie/atu/CreateFile.java
+++ src/main/java/ie/atu/CreateFile.java
@@ -1,20 +1,30 @@
 package ie.atu;
 import java.io.*;
-
+import java.util.Scanner;
 
 public class CreateFile {
     public static void main(String[] args) throws IOException {
 
         {
-
-            File myFile = new File("MyFile.txt");
+            Scanner input = new Scanner(System.in);
+            System.out.println("Please enter name of file");
+            String filename = input.nextLine();
+            File myFile = new File(filename);
             myFile.createNewFile();
             System.out.println("My file is located at " +myFile.getAbsolutePath());
             String info = "hello";
             try{
-                PrintWriter outputWriter = new PrintWriter(new FileWriter (myFile,true));
-                outputWriter.println("good");
+
+                PrintWriter outputWriter = new PrintWriter(new FileWriter (filename,true));
+                outputWriter.println("bye");
                 outputWriter.println();
+                outputWriter.close();
+                Scanner input1 = new Scanner(new File(filename));
+                while(input1.hasNext()){
+                    String str1 = input1.nextLine();
+                    System.out.println(str1);
+                }
+                input1.close();
 
             }
             catch (IOException e){
